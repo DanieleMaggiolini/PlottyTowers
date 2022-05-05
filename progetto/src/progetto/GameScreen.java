ac987139ec4eb4e5a9dcf21d410ef01446992bc6
whMino <mattimino04@gmail.com>
2022-05-05 23:12:53 +0200

    Revert "Completamento documentazione del package "progetto""
    This reverts commit f1c2fa2215fbfaf38a8884708596e6879ce44964.

--- progetto/src/progetto/GameScreen.java
+++ progetto/src/progetto/GameScreen.java
@@ -1,13 +1,4 @@
-/**
-* @author  Daniele Maggiolini
-* @author  Mattia Minotti
-* @version 0.0
-* @file GameScreen.java 
-* 
-* @brief file della classe JPanel GameScreen che gestisce tutte le print 
-* che saranno eseguite nella schermata di gioco e gli imput da tastiera e mouse
-*
-*/
+
 package progetto;
 
 import inputs.KeyboardListener;
@@ -18,41 +9,27 @@ import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import javax.swing.JPanel;
 
-/** 
-* @class GameScreen
-* 
-* @brief JFrame JPanel gestisce le print e i comandi input.
-* 
-*/ 
-
 public class GameScreen extends JPanel{
     
-    //oggeto del game
     private Game game;
-    
-    //dimensioni da applicare al panel
+   
     private Dimension size;
     
-   //oggetto lettura imput mouse
+   
     private MyMouseListener mymouselistener;
-    
-    //oggetto lettura imput tastiera
     private KeyboardListener keyboardlistener;
     
-    /**
-     @brief costruttore setta il game e richiama il metodo per impostare la size.
-     * 
-     *@param game
-     */
+    //costruttore (@param buffer di immagini)  
     public GameScreen(Game game){
         this.game = game;
         
+        
         setPanelSize();
+        
+        
+        
     }
     
-    /**
-     * @brief inizializza gli oggetti presenti nel file.
-     */
     public void initImputs() {
         mymouselistener = new MyMouseListener(game);
         keyboardlistener = new KeyboardListener(game);
@@ -64,9 +41,7 @@ public class GameScreen extends JPanel{
         requestFocus();
     }
     
-    /**
-     @brief imposta le dimensioni del panel.
-     */
+    //metodo per impostale le dimensioni della schermata
     private void setPanelSize() {
         size = new Dimension(Game.currentScreenWidth, Game.currentScreenHeight);//1792.1010
         setMinimumSize(size);
@@ -74,9 +49,6 @@ public class GameScreen extends JPanel{
         setMaximumSize(size);
     }
     
-    /**
-     @brief stampa le componenti e richiama il render
-     */
     @Override
     public void paintComponent(Graphics g){
         super.paintComponent(g);
@@ -84,4 +56,8 @@ public class GameScreen extends JPanel{
         game.getRender().render(g);
         
     }
+    
+    
+    
+    
 }
--- progetto/src/progetto/GameStates.java
+++ progetto/src/progetto/GameStates.java
@@ -1,26 +1,10 @@
-/**
-* @author  Daniele Maggiolini
-* @author  Mattia Minotti
-* @version 0.0
-* @file GameStates.java 
-* 
-* @brief file d'enumerazione a scopo di semplificare la modifica della schermata
-* visualizzata
-*
-*/
 package progetto;
 
 public enum GameStates {
     PLAYING, MENU, SETTINGS, EDIT, LVL1;
     
-    //oggetto gi questa classe inizializzato con il menu
     public static GameStates gamestates = MENU;
     
-    /**
-     @brief costruttore setta il gamestates in base al parametro passato.
-     * 
-     *@param state schermata da settare
-     */
     public static void setGameState(GameStates state){
         gamestates=state;
     }
